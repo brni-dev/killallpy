2cd533ca859a28e8aa0ccddf3630de9ab4c380d6
brni-dev <64311604+brni-dev@users.noreply.github.com>
2021-03-21 18:20:59 +0100

    Update killall.py

--- killall.py
+++ killall.py
@@ -1,7 +1,9 @@
-import os, subprocess, time
+import os, subprocess, time, argparse
 
-thingtokill="spotify" # Make this anything u want to kill
-print("Loading...")
+# Adds command parser
+parser = argparse.ArgumentParser(description='Kill all processes with the term')
+parser.add_argument('process', metavar='processName', type=str, help='process name')
+arg = parser.parse_args().process.lower()
 
 # Determine whether or not the user has a SHELL environment variable set/Is running script on a non-Linux machine
 SHELL=os.getenv('SHELL')
@@ -12,15 +14,10 @@ if SHELL==None:
 cmdoutput=str(subprocess.check_output("ps -eo pid,command",shell=True).decode("utf-8")).split()
 last=""
 
-print(f"Loaded! Terminating {thingtokill} processes\n")
-
 # Searches for the thingtokill value in each case-lowered element from the command output array
 for process in cmdoutput:
-    if thingtokill in str(process).lower():
+    if arg in str(process).lower():
         time.sleep(0.04)
         os.system("kill -kill " + last)
-        print("Killed {thingtokill} process " + str(last))
     else:
         last=str(process)
-
-print("\nDone! Try relaunching {thingtokill}!")
